1dbd26aa770b583b4463a038b2efd5f0bd17a3b8
Paulius <pauliuspe@wix.com>
2021-07-22 15:04:22 +0300

    game done, optimisation needed

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const info = document.querySelector('.option');
 
 let counter = 0;
 let state = 1;
+let winner;
 
 takeOne.addEventListener("click", () => {
     info.innerHTML = "One flag was removed";
@@ -17,6 +18,7 @@ takeOne.addEventListener("click", () => {
     counter++;
     state++;
     whosTurn();
+    whoWon();
 });
 
 takeTwo.addEventListener("click", () => {
@@ -25,12 +27,12 @@ takeTwo.addEventListener("click", () => {
     counter += 2;
     state++;
     whosTurn();
+    whoWon();
 });
 
 reset.addEventListener("click", () => {
     info.innerHTML = "How many flags do you wish to take?";
     resetAll();
-    state = 1;
     whosTurn();
 });
 
@@ -44,15 +46,21 @@ function removeOne(counter) {
 
 function removeTwo(counter) {
     flags.forEach(flag => {
-        flags[counter].style.display = 'none';
-        flags[counter + 1].style.display = 'none';
+        if (counter < 20) {
+            flags[counter].style.display = 'none';
+            flags[counter + 1].style.display = 'none';
+        } else { flags[counter].style.display = 'none'; }
     })
 };
 
 function resetAll() {
+    state = 1;
     counter = 0;
     flags.forEach(flag => {
         flag.style.display = 'initial';
+        document.querySelector('.gamebox__winner').innerHTML = "";
+        takeOne.style.display = 'initial';
+        takeTwo.style.display = 'initial';
     })
 };
 
@@ -63,5 +71,28 @@ function whosTurn() {
     } else if (!state % 2 == 0 || state == 1) {
         turn.style.color = 'blue';
         turn.innerHTML = "Your turn";
+    }
+};
+
+function whosLast() {
+    if (flags[19].style.display === 'none' && turn.innerHTML === "Opponent's turn") {
+        winner = false;
+    } else if (flags[19].style.display === 'none' && turn.innerHTML === "Your turn") {
+        winner = true;
+    }
+};
+
+function whoWon() {
+    whosLast();
+    if (flags[20].style.display === 'none') {
+        turn.innerHTML = "Reset to play again";
+        info.innerHTML = "No flags left";
+        takeOne.style.display = 'none';
+        takeTwo.style.display = 'none';
+        if (winner == false) {
+            document.querySelector('.gamebox__winner').innerHTML = "You WIN!";
+        } else if (winner == true) {
+            document.querySelector('.gamebox__winner').innerHTML = "Opponent WINS!";
+        }
     }
 };
